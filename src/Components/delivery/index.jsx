1c21d4632a3021d2f28e9f615b30bf2dc7c6c489
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-05-22 11:52:30 +0530

    filter

--- src/Components/delivery/index.jsx
+++ src/Components/delivery/index.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import Filters from '../common/filters'
 import "./delivery.css"
 
+
+const deliveryFilters = [
+  {
+    id:1,
+    icon: <i className="fi fi-rr-settings-sliders absolute-center" ></i>,
+    title: "Filters",
+  },
+  {
+    id:2,
+    title: "Rating: 4.0+",
+  },
+  {
+    id:3,
+    title: "Safe and Hygienic",
+  },
+  {
+    id:4,
+    title: "pure Veg",
+  },
+
+  {
+    id:5,
+    icon: <i className ="fi fi-br-sort-alt absolute-center"></i>,
+    title: "Delivery Time",
+  },
+  {
+    id:6,
+    title: "Great Offers",
+  },
+
+]
 const Deliver = () => {
   return (
     <div>
-      This is Delivery Components
+    <div className='max-width'>
+    <Filters filterList = {deliveryFilters}/>
+    </div>
+      
     </div>
   )
 }
